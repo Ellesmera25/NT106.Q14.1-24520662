d7a08c6af2fc36c9e8b5492d8389ba8d432fb83b
Ellesmera25 <24520662@gm.uit.edu.vn>
2025-10-08 20:10:17 +0700

    Update Form5.Designer.cs

--- Lab01/Form5.Designer.cs
+++ Lab01/Form5.Designer.cs
@@ -99,7 +99,6 @@
             button1.TabIndex = 11;
             button1.Text = "Thêm";
             button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
             // 
             // button2
             // 
